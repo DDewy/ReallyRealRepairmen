3b6ed3b5f0355a50ba32f5b83eb764d9502d9163
fucking why <strachly@gmail.com>
2020-02-01 22:29:17 +0000

    TV Breaks Twice with different Fix

---
+++ Assets/Scripts/Fixables/TVScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TVScript : MonoBehaviour 
+{
+
+	// Use this for initialization
+	private int fixNum = 0;
+	public void FixTV()
+	{
+		fixNum++;
+
+		if (fixNum == 2)
+		{
+			gameObject.GetComponent<MeshRenderer>().enabled = true;
+		}
+	}
+
+}
