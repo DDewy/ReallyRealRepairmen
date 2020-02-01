2dcc1b41ce0fd9b1cf3e7dea97be246a7e50107a
Viv <strachly@gmail.com>
2020-02-01 16:22:39 +0000

    Repairables Enum for Dave
    MORE MANAGERS!!! MORE MANAGERS, DAVE

---
+++ Assets/Scripts/RepairablesEnum.cs
@@ -0,0 +1,5 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum Repairables{TV, Toilet, Pipes, Sink, Balcony, Window, Conservatory, Boiler, Oven, Fridge, Car}
