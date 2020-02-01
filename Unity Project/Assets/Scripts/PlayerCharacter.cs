804f9ac55ae65b531917e1fc111e10ee9b131e43
Cercain <strachly@gmail.com>
2020-02-01 10:50:53 +0000

    Structure Change 2.0

--- Unity Project/Assets/Scripts/PlayerCharacter.cs
+++ Unity Project/Assets/Scripts/PlayerCharacter.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class PlayerCharacter : MonoBehaviour {
 
     [Header("Components")]
-    public Camera _cam;
-    public Rigidbody _rb;
+    public Camera Cam;
+    public Rigidbody RB;
+
+    [Header("Movement")]
+    public float MovementSpeed = 1f;
+
+    [Header("Camera Settings")]
+    public float PitchSensitivity = 1f;
+    public float YawSensitivity = 1f;
+    public bool PitchInvert = false;
+    public bool YawInvert = false;
 
     private Transform _camTrans;
+    private Transform _playerTrans;
+    private Vector3 _movementDelta;
 
 	// Use this for initialization
 	void Start ()
     {
-        _camTrans = _cam.transform;
+        _camTrans = Cam.transform;
+        _playerTrans = RB.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
@@ -22,6 +37,34 @@ public class PlayerCharacter : MonoBehaviour {
         float xMove = Input.GetAxis("Horizontal");
         float yMove = Input.GetAxis("Vertical");
 
+        float xMouse = Input.GetAxis("Mouse X");
+        float yMouse = Input.GetAxis("Mouse Y");
 
+        //Calculate the player movement
+        _movementDelta += xMove * _playerTrans.right;
+        _movementDelta += yMove * _playerTrans.forward;
+        _movementDelta *= MovementSpeed * Time.deltaTime;
+        
+        //Calculate the camera rotation
+        Quaternion playerRot = _playerTrans.localRotation;
+        Quaternion camRot = _camTrans.localRotation;
+
+        playerRot *= Quaternion.Euler(Vector3.up * (xMouse * YawSensitivity * (YawInvert ? -1f : 1f)));
+        camRot *= Quaternion.Euler(Vector3.right * (yMouse * PitchSensitivity * (PitchInvert ? 1f : -1f)));
+
+        //Set the player position
+        _playerTrans.localRotation = playerRot;
+        _camTrans.localRotation = camRot;
+    }
+
+    private void FixedUpdate() 
+    {
+        RB.MovePosition(RB.position + _movementDelta);
+        _movementDelta = Vector3.zero;
+    }
+
+    private void OnApplicationQuit() {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 }
