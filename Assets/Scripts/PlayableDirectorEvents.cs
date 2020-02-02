5cb6c48401c38fbf1625cdfbdae3dc8ee2cac0cf
DDewy <ExtremeDewy@gmail.com>
2020-02-02 15:15:52 +0000

    Added Credits and Jerry ending things

---
+++ Assets/Scripts/PlayableDirectorEvents.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Events;
+
+public class PlayableDirectorEvents : MonoBehaviour {
+
+	public PlayableDirector director;
+
+	public UnityEvent OnFinished = new UnityEvent();
+
+	private bool hasPlayed = false;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(hasPlayed == false)
+		{
+			if(director.state == PlayState.Playing)
+			{
+				hasPlayed = true;
+			}
+		}
+		else
+		{
+			if(director.state == PlayState.Paused)
+			{
+				//Has finished
+				Debug.Log("Director finished");
+				this.enabled = false;
+				OnFinished.Invoke();
+			}
+		}
+	}
+}
