9a09b301e225b7cd50d0ee598a119e816e749148
fucking why <strachly@gmail.com>
2020-02-02 00:20:34 +0000

    AAAAA task things

--- Assets/Scripts/Fixables/RepairablesEnum.cs
+++ Assets/Scripts/Fixables/RepairablesEnum.cs
@@ -2,4 +2,5 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Repairables{TV, Toilet, Pipes, Sink, Balcony, Window, Conservatory, Boiler, Oven, Fridge, Car}
+public enum Repairables{TV, Toilet, Pipes, Sink, Balcony, Window, Conservatory, Boiler, Oven, Fridge, Car, Jerry}
+//Jerry = entering trigger volume in conservatory
