babe028de6bd1b8e1ca0983871c169b4a6a46e2d
david_dewhirst <david.dewhirst1996@gmail.com>
2020-02-03 10:51:28 +0000

    Baking Lighting for the scene and added Jerry talking animation

--- Assets/Scripts/TriggerEvents.cs
+++ Assets/Scripts/TriggerEvents.cs
@@ -8,11 +8,23 @@ public class TriggerEvents : MonoBehaviour {
 	public UnityEvent TriggerEnter = new UnityEvent();
 	public UnityEvent TriggerExit = new UnityEvent();
 
+	public UnityEvent CollisionEnter = new UnityEvent();
+
+	public UnityEvent CollisionExit = new UnityEvent();
+
 	private void OnTriggerEnter(Collider other) {
 		TriggerEnter.Invoke();
 	}
 
 	private void OnTriggerExit(Collider other) {
 		TriggerExit.Invoke();
+	}
+
+	private void OnCollisionEnter(Collision other) {
+		CollisionEnter.Invoke();
+	}
+
+	private void OnCollisionExit(Collision other) {
+		CollisionExit.Invoke();
 	}
 }
